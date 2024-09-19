339ec29848c4477f446e8168da7162ddaccc24c2
Pedro-Wilker <pedrowilker1516@gmail.com>
2024-09-19 16:12:11 -0300

    Fazendo novas atualizacoes

--- js/login.js
+++ js/login.js
@@ -1,21 +1,43 @@
-const form = document.querySelector('form');
-const emailInput = document.getElementById('email');
-const passwordInput = document.getElementById('password');
+document.addEventListener('DOMContentLoaded', function () {
+  const loginForm = document.querySelector('form');
 
-form.addEventListener('submit', login);
+  loginForm.addEventListener('submit', function (event) {
+      event.preventDefault();
 
-function login(event) {
-  event.preventDefault();
+      const email = document.querySelector('input[name="email"]').value;
+      const password = document.querySelector('input[name="password"]').value;
 
-  const email = emailInput.value;
-  const password = passwordInput.value;
+      if (validateLogin(email, password)) {
+          alert('Login bem-sucedido!');
+          window.location.href = '../home.html'; 
+      } else {
+          alert('Email ou senha inválidos!');
+      }
+  });
 
-  const profile = JSON.parse(localStorage.getItem('profile'));
+document.getElementById('cadastro-form').addEventListener('submit', function(event) {
+    event.preventDefault();
 
-  if (profile && email === profile.email && password === profile.password) {
-    window.location.href = 'perfil.html';
-  } else {
-    alert('Email ou senha incorretos. Tente novamente.');
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+    const name = document.getElementById('name').value;
+    const sobrenome = document.getElementById('sobrenome').value;
+
+    registerUser(email, password, name, sobrenome);
+});
+
+  function validateLogin(email, password) {
+      const storedEmail = localStorage.getItem('userEmail');
+      const storedPassword = localStorage.getItem('userPassword');
+
+      return email === storedEmail && password === storedPassword;
   }
-  window.location.href = '../pages/index.html';
+
+  function registerUser(email, password, name, sobrenome) {
+    localStorage.setItem('userEmail', email);
+    localStorage.setItem('userPassword', password);
+    localStorage.setItem('userName', name);
+    localStorage.setItem('userSobrenome', sobrenome);
 }
+
+});
